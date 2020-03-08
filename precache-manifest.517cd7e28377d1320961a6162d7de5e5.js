911fe5455016b6749ad6324ffc78c1bc4b79797f
chenshyin <shuyin_chen@shannonai.com>
2020-03-08 22:36:45 +0800

    FEA/init

---
+++ precache-manifest.517cd7e28377d1320961a6162d7de5e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252af6239a57581403887a8b868ae241",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a37548a15197595b2f0c",
+    "url": "./static/css/2.fa00d1fc.chunk.css"
+  },
+  {
+    "revision": "4bedb619aab64ac597a5",
+    "url": "./static/css/main.f56d426c.chunk.css"
+  },
+  {
+    "revision": "a37548a15197595b2f0c",
+    "url": "./static/js/2.7ee84ee4.chunk.js"
+  },
+  {
+    "revision": "eda28acf3a81e599a50b8f4a39550a71",
+    "url": "./static/js/2.7ee84ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bedb619aab64ac597a5",
+    "url": "./static/js/main.80ab9752.chunk.js"
+  },
+  {
+    "revision": "25474e61e91afa125ec8",
+    "url": "./static/js/runtime-main.1c48b971.js"
+  },
+  {
+    "revision": "1cb9e662760ed0abd4e80df4d914e2b3",
+    "url": "./static/media/gbqq.1cb9e662.mp3"
+  }
+]);
